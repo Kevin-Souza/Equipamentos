f47031d7dcb5ae802f65a3025a5b3beb06744598
Kevin-Souza <78280970+Kevin-Souza@users.noreply.github.com>
2021-10-18 22:00:50 -0300

    Bootstrap

--- Equipamentos/Controllers/CategoriasController.cs
+++ Equipamentos/Controllers/CategoriasController.cs
@@ -1,7 +1,9 @@
-﻿using Equipamentos.Models;
+﻿using Equipamentos.Context;
+using Equipamentos.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,6 +11,8 @@ namespace Equipamentos.Controllers
 {
     public class CategoriasController : Controller
     {
+        private EFContext context = new EFContext();
+
         private static IList<Categoria> categorias = new List<Categoria>()
         {
             new Categoria()
@@ -44,6 +48,67 @@ namespace Equipamentos.Controllers
             categoria.CategoriaID = categorias.Select(m => m.CategoriaID).Max() + 1;
             categorias.Add(categoria);
             return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit (long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categoria categoria = context.categorias.Find(id);
+            if (categoria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categoria);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Entry(categoria).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(categoria);
+        }
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categoria categoria = context.categorias.Find(id);
+            if (categoria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categoria);
+        }
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categoria categoria = context.categorias.Find(id);
+            if (categoria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categoria);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(long id)
+        {
+            Categoria categoria = context.categorias.Find(id);
+            context.categorias.Remove(categoria);
+            context.SaveChanges();
+            return RedirectToAction("index");
         }
     }
 }
